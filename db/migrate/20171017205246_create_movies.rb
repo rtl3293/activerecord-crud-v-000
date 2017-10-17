26bff2e3844130f111de3bebd878e8c9ceab66f4
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-17 20:56:15 +0000

    automatically backed up by learn

--- db/migrate/20171017205246_create_movies.rb
+++ db/migrate/20171017205246_create_movies.rb
@@ -1,5 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
   end
 end
